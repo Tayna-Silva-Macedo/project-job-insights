e7db9f741295c6ff20a97496638a90407d393b88
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-01-23 19:26:01 -0300

    test: implementação do teste da função sount_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    words_list = ["python", "Python", "javascript", "Javascript"]
+    expected_list = [1639, 1639, 122, 122]
+
+    for index in range(len(words_list)):
+        assert (
+            count_ocurrences(path, words_list[index]) == expected_list[index]
+        )
