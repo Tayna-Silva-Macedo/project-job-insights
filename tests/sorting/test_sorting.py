03449b13a6dda2a0c1db6e7835a5d351782d8fb5
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-01-23 20:20:47 -0300

    test: implementação do teste da função sort_by_criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,44 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+import pytest
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture()
+def jobs_mock():
+    return [
+        {"min_salary": 100, "max_salary": 1000, "date_posted": "2020-06-10"},
+        {"min_salary": 50, "max_salary": 2000, "date_posted": ""},
+        {"min_salary": "", "max_salary": 3000, "date_posted": "2020-05-24"},
+        {"min_salary": 200, "max_salary": "", "date_posted": "2020-05-02"},
+    ]
+
+
+def test_sort_by_criteria(jobs_mock):
+    sorted_by_min = [
+        {"min_salary": 50, "max_salary": 2000, "date_posted": ""},
+        {"min_salary": 100, "max_salary": 1000, "date_posted": "2020-06-10"},
+        {"min_salary": 200, "max_salary": "", "date_posted": "2020-05-02"},
+        {"min_salary": "", "max_salary": 3000, "date_posted": "2020-05-24"},
+    ]
+
+    sort_by(jobs_mock, "min_salary")
+    assert jobs_mock == sorted_by_min
+
+    sorted_by_max = [
+        {"min_salary": "", "max_salary": 3000, "date_posted": "2020-05-24"},
+        {"min_salary": 50, "max_salary": 2000, "date_posted": ""},
+        {"min_salary": 100, "max_salary": 1000, "date_posted": "2020-06-10"},
+        {"min_salary": 200, "max_salary": "", "date_posted": "2020-05-02"},
+    ]
+
+    sort_by(jobs_mock, "max_salary")
+    assert jobs_mock == sorted_by_max
+
+    sorted_by_date = [
+        {"min_salary": 100, "max_salary": 1000, "date_posted": "2020-06-10"},
+        {"min_salary": "", "max_salary": 3000, "date_posted": "2020-05-24"},
+        {"min_salary": 200, "max_salary": "", "date_posted": "2020-05-02"},
+        {"min_salary": 50, "max_salary": 2000, "date_posted": ""},
+    ]
+
+    sort_by(jobs_mock, "date_posted")
+    assert jobs_mock == sorted_by_date
