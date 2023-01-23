a8b7a80adaf9e1cbbdfd5d2319b48458541b8a4f
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-01-23 19:45:37 -0300

    test: implementação do teste da função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,15 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    expected_keys = {"title", "salary", "type"}
+
+    dict_jobs = read_brazilian_file(path)
+
+    keys = set()
+    for job in dict_jobs:
+        for key in job.keys():
+            keys.add(key)
+
+    assert keys == expected_keys
